dd19f9635bdaef6dc040115a04380f6e81182764
Zhang Yang <260328360@qq.com>
2019-09-20 23:34:39 +0800

    * implement IconProvider Standard Icon

--- mc/tools/IconProvider.py
+++ mc/tools/IconProvider.py
@@ -2,6 +2,9 @@ from PyQt5.Qt import QImage
 from PyQt5.Qt import QIcon
 from threading import Lock
 from mc.common.designutil import Singleton
+from PyQt5.Qt import QStyle
+from PyQt5.QtWidgets import QApplication
+from PyQt5.Qt import Qt
 
 # Need to be QWidget subclass, otherwise qproperty- setting won't work
 # but in Python could inherate form object directly
@@ -43,7 +46,39 @@ class IconProvider(Singleton):
         @param: icon QStyle::StandardPixmap
         @return: QIcon
         '''
-        return QIcon()
+        defIcon = QApplication.style().standardIcon(icon)
+        if icon == QStyle.SP_MessageBoxCritical:
+            return QIcon.fromTheme('dialog-error', defIcon)
+        elif icon == QStyle.SP_MessageBoxInformation:
+            return QIcon.fromTheme('dialog-information', defIcon)
+        elif icon == QStyle.SP_MessageBoxQuestion:
+            return QIcon.fromTheme('dialog-question', defIcon)
+        elif icon == QStyle.SP_MessageBoxWarning:
+            return QIcon.fromTheme('dialog-warning', defIcon)
+        elif icon == QStyle.SP_DialogCloseButton:
+            return QIcon.fromTheme('dialog-close', defIcon)
+        elif icon == QStyle.SP_BrowserStop:
+            return QIcon.fromTheme('progress-stop', defIcon)
+        elif icon == QStyle.SP_BrowserReload:
+            return QIcon.fromTheme('view-refresh', defIcon)
+        elif icon == QStyle.SP_FileDialogToParent:
+            return QIcon.fromTheme('go-up', defIcon)
+        elif icon == QStyle.SP_ArrowUp:
+            return QIcon.fromTheme('go-up', defIcon)
+        elif icon == QStyle.SP_ArrowDown:
+            return QIcon.fromTheme('go-down', defIcon)
+        elif icon == QStyle.SP_ArrowForward:
+            if QApplication.layoutDirection() == Qt.RightToLeft:
+                return QIcon.fromTheme('go-previous', defIcon)
+            else:
+                return QIcon.fromTheme('go-next', defIcon)
+        elif icon == QStyle.SP_ArrowBack:
+            if QApplication.layoutDirection() == Qt.RightToLeft:
+                return QIcon.fromTheme('go-next', defIcon)
+            else:
+                return QIcon.fromTheme('go-previous', defIcon)
+        else:
+            return defIcon
 
     @classmethod
     def newTabIcon(cls):
